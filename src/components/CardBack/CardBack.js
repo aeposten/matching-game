5de6e9529804ba666f4179c03f986227ee74dab3
Amy <86963506+aeposten@users.noreply.github.com>
2023-02-10 15:11:39 -0800

    Added card back

---
+++ src/components/CardBack/CardBack.js
@@ -0,0 +1,9 @@
+import "./CardBack.css"
+function CardBack() {
+    return (
+        <section>
+            Check out the back of the card!
+        </section>
+    )
+}
+export default CardBack;
--- src/components/CardGrid/CardGrid.js
+++ src/components/CardGrid/CardGrid.js
@@ -1,4 +1,5 @@
 import Card from "../Card/Card";
+import CardBack from "../CardBack/CardBack";
 
 import "./CardGrid.css";
 
